34623fbe8d02be8dc95a784ac3f19620234f9284
Asaf Kostiner <kostinerasaf@gmail.com>
2021-12-30 16:13:52 +0200

    init git

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask_session import Session
+from flask import Flask, request, session, render_template, send_file
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import redirect
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'test'
+app.config["SESSION_PERMANENT"] = False
+app.config['SESSION_TYPE'] = 'filesystem'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+# app.config['SQLALCHEMY_BINDS'] = {'broadcast': 'sqlite:///data.db'}
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+sess = Session(app)
+
+
+# db = SQLAlchemy(app)
+
+
+# class Users(db.Model):
+#     # to init db in terminal type: python ->from app import db->db.create_all()-> exit(). and you are set!
+#     email = db.Column(db.String(200), primary_key=True)
+#     password = db.Column(db.String(200), nullable=False)
+#     twilioAuthToken = db.Column(db.String(200), nullable=True)
+#     twilioAccountSid = db.Column(db.String(200), nullable=True)
+#     twilioMessagingServiceSid = db.Column(db.String(200), nullable=True)
+#     blacklist = db.Column(db.JSON)
+#     generalList = db.Column(db.JSON)
+#     expiredList = db.Column(db.JSON)
+#     expiringList = db.Column(db.JSON)
+#     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+#
+#     def __repr__(self):
+#         return '<Name %r>' % self.id
+#
+#
+# @app.before_first_request
+# def create_tables():
+#      db.create_all()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def baseRoute():
+    return redirect("/home")
+
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    return render_template("home.html", l=10)
+
+
+if __name__ == "__main__":
+    app.run()
