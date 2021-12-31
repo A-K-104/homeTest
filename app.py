d55ffa7a67535af97b34f252bb6a0c00d43a3845
Asaf Kostiner <kostinerasaf@gmail.com>
2021-12-31 02:10:21 +0200

    added db and adding funciton

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import json
+
 from flask_session import Session
 from flask import Flask, request, session, render_template, send_file
 from flask_sqlalchemy import SQLAlchemy
@@ -10,35 +12,34 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'test'
 app.config["SESSION_PERMANENT"] = False
 app.config['SESSION_TYPE'] = 'filesystem'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-# app.config['SQLALCHEMY_BINDS'] = {'broadcast': 'sqlite:///data.db'}
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_BINDS'] = {'broadcast': 'sqlite:///data.db'}
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 sess = Session(app)
 
+db = SQLAlchemy(app)
 
-# db = SQLAlchemy(app)
+
+class Status_db(db.Model):
+    # to init db in terminal type: python ->from app import db->db.create_all()-> exit(). and you are set!
+    name = db.Column(db.String(200), primary_key=True)
+
+    def __repr__(self):
+        return '{' + f'"name": "{self.name}"' + '}'
 
 
-# class Users(db.Model):
-#     # to init db in terminal type: python ->from app import db->db.create_all()-> exit(). and you are set!
-#     email = db.Column(db.String(200), primary_key=True)
-#     password = db.Column(db.String(200), nullable=False)
-#     twilioAuthToken = db.Column(db.String(200), nullable=True)
-#     twilioAccountSid = db.Column(db.String(200), nullable=True)
-#     twilioMessagingServiceSid = db.Column(db.String(200), nullable=True)
-#     blacklist = db.Column(db.JSON)
-#     generalList = db.Column(db.JSON)
-#     expiredList = db.Column(db.JSON)
-#     expiringList = db.Column(db.JSON)
-#     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-#
-#     def __repr__(self):
-#         return '<Name %r>' % self.id
-#
-#
-# @app.before_first_request
-# def create_tables():
-#      db.create_all()
+class Transition_db(db.Model):
+    name = db.Column(db.String(200), primary_key=True)
+    from_user = db.Column(db.String(200))
+    to_user = db.Column(db.String(200))
+
+    def __repr__(self):
+        return '{' + f'"name": "{self.name}",' + f'"from_user": "{self.from_user}",' + f'"to_user": "{self.to_user}"' + '}'
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -48,13 +49,28 @@ def baseRoute():
 
 @app.route('/home', methods=['GET', 'POST'])
 def home():
-    statuses = [Status("name")]
-    transitions = [Transition("name", "user", "to user")]
+    names = Status_db.query.all()
+    trans = Transition_db.query.all()
+    statuses = []
+    transitions = []
+    for name in names:
+        temp = json.loads(str(name))
+        statuses.append(Status(temp['name']))
+    for tran in trans:
+        temp = json.loads(str(tran))
+        transitions.append(Transition(temp['name'], temp['from_user'], temp['to_user']))
     if request.method == "POST":
         if request.form.__contains__('name'):
+            status = Status_db(name=request.form['name'])
+            db.session.add(status)
             statuses.append(Status(request.form['name']))
         elif request.form.__contains__('transition'):
-            statuses.append(Status(request.form['name']))
+            transition = Transition_db(name=request.form['transition'], from_user=request.form['from_user'],
+                                       to_user=request.form['to_user'])
+            db.session.add(transition)
+            transitions.append(Transition(request.form['transition'], request.form['from_user'],
+                                          request.form['to_user']))
+        db.session.commit()
     return render_template("home.html", statuses=statuses, transitions=transitions)
 
 
--- classes/transition.py
+++ classes/transition.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Transition:
     def __init__(self, name: str, from_user: str, to_user: str):
         self.name = name
@@ -5,13 +8,18 @@ class Transition:
         self.to_user = to_user
 
     def get_map(self):
-        return {'name' + ":'" + self.name + "'" +
-                'from_user' + ":'" + self.from_user + "'" +
-                'to_user' + ":'" + self.to_user + "'"
-                }
+        return '{"name"' + ':"' + self.name + '"' +\
+               ', "from_user"' + ':"' + self.from_user +\
+               '"' + ', "to_user"' + ':"' + self.to_user + '"}'
 
 
 def decode_json(json_str):
     return Transition(json_str['name'], json_str['from_user'], json_str['to_user'])
 
 
+# transitions = Transition("name1", "user1", "to user1")
+# print(transitions.get_map()) # '{"first_name": "Michael", "last_name": "Rodgers", "department": "Marketing"}'
+# a = json.loads(str(transitions.get_map()))
+# i = {'name':'name1', 'from_user':'user1', 'to_user':'to user1'}
+# print(a)
+# print(i['name'])
